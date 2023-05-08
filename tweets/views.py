e601d5ca479c5b16f3d740b23b1ddaf837768f12
Ricardo Montemayor <ricardomongza@gmail.com>
2023-05-07 18:01:55 -0600

    Add home template

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('hello, here go the tweets')
+    return render(request, 'tweets/home.html')
