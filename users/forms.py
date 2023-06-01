d78cf4be1eaffc7f3cec6517f7aa39f9a012418d
AndresRamirez0394 <A01720068@itesm.mx>
2023-06-01 16:33:06 -0600

    Register page

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,7 @@ class RegistreForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import RegistreForm
+from .forms import RegistreForm, LoginForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -12,3 +13,21 @@ def register(request):
     else:
         form = RegistreForm()
     return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+
+    context = {'form': form}
+    return render(request, 'login.html', context)
