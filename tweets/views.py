5105282fb270f44dd8adecfaac34e172e7a41f00
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-01 15:09:34 -0600

    Display tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Tweet
 
+def display_tweets(request):
+    tweets = Tweet.objects.order_by('-created_at')[:10]
 
-def home(request):
-    return render(request, 'tweets/home.html')
+    return render(request, 'tweets/tweets.html', {'tweets': tweets})
