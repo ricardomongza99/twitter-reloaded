3b497bbe40773723012bbe4daa1c0ee286c18e96
AndresRamirez0394 <A01720068@itesm.mx>
2023-06-01 16:16:56 -0600

    login page working

--- twitter_reloaded/urls.py
+++ twitter_reloaded/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweets/', include('tweets.urls')),
+    path('register/', register, name='register'),
 ]
