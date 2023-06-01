d1f2766334a8813eb01e89d31f82d28be674d528
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-01 17:19:07 -0600

    Add create tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tweet
+from django.utils import timezone
 
-def display_tweets(request):
+
+# @login_required
+def home(request):
     tweets = Tweet.objects.order_by('-created_at')[:10]
+    return render(request, 'tweets/home.html', {'tweets': tweets})
 
-    return render(request, 'tweets/tweets.html', {'tweets': tweets})
+
+# @login_required
+def create_tweet(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        user = request.user
+        created_at = timezone.now()
+
+        tweet = Tweet(content=content, user=user, created_at=created_at)
+        tweet.save()
+
+        return redirect('home')
+    else:
+        return redirect('home')
