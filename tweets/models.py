14db4a1ef80633fe78b6efdcd30bf45ec632b711
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-01 13:57:34 -0600

    Add User, Tweet and Reply models

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Reply(models.Model):
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, related_name="reply", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
