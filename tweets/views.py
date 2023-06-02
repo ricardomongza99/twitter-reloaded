e3b9c45597c0bbbf1c42d3d070c45168691df57b
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-01 19:26:05 -0600

    Update tweet models

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tweet
 from django.utils import timezone
 
 
 # @login_required
 def home(request):
-    tweets = Tweet.objects.order_by('-created_at')[:10]
+    tweets = Tweet.objects.filter(parent__isnull=True).order_by('-created_at')[:10]
     return render(request, 'tweets/home.html', {'tweets': tweets})
 
 
@@ -22,3 +22,9 @@ def create_tweet(request):
         return redirect('home')
     else:
         return redirect('home')
+
+
+def tweet_thread(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    replies = Tweet.objects.filter(parent=tweet)
+    return render(request, 'tweets/thread.html', {'main_tweet': tweet, 'replies': replies})
