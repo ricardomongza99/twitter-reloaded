57f65584dd59cb741471ffdf1486c9bd66a38ddf
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-02 18:39:18 -0600

    Update urls

--- twitter_reloaded/urls.py
+++ twitter_reloaded/urls.py
@@ -20,6 +20,7 @@ from users.views import register, user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tweets.urls')),
     path('tweets/', include('tweets.urls')),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
