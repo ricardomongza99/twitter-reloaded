c54d89ef187cb2f81cd8c5136f288e820667ed66
AndresRamirez0394 <A01720068@itesm.mx>
2023-06-02 22:16:21 -0600

    Login and Register Unit Tests

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from users import views as userviews
 
+app_name = 'tweets'
 
 urlpatterns = [
     path('', views.home, name='home'),
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-# Create your tests here.
+class UserRegistrationCase(TestCase):
+    def test_registration(self):
+
+    #create a test user
+        username = 'testuser'
+        password = 'testpassword'
+        email = 'testuser@example.com'
+
+        User.objects.create_user(username=username, password=password, email=email)
+
+    # Retrieve the user from the database
+        user = User.objects.get(username=username)
+
+    # Assert that the user exists in the database
+        self.assertIsNotNone(user)
+
+        pass
+
+class UserLoginTest(TestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def test_correct_credentials(self):
+        user = authenticate(username=self.username, password=self.password)
+        self.assertIsNotNone(user)
+        self.assertEqual(user, self.user)
+
+    def test_incorrect_credentials(self):
+        user = authenticate(username=self.username, password='wrongpassword')
+        self.assertIsNone(user)
