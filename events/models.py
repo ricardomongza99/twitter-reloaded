9f90649eeac2bf4546d51279d7c115bcecacacb2
Ricardo Montemayor <ricardomongza@gmail.com>
2023-06-02 19:15:08 -0600

    Update Event model

--- events/models.py
+++ events/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Event(models.Model):
-    ACTION_CHOICES = (
+    EVENT_TYPES = (
         ('CT', 'Create Tweet'),
         ('RT', 'Reply Tweet'),
         ('OA', 'Open Application'),
     )
-    action = models.CharField(max_length=2, choices=ACTION_CHOICES)
+    type = models.CharField(max_length=2, choices=EVENT_TYPES)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
